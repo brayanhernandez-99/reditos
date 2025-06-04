eb49d749131c5aa49144be933346b36b7ec8f39f
brayanhernandez-99 <bhrestrepo_27@hotmail.com>
2025-06-03 23:11:35 -0500

    Update Terraform

--- terraform/modules/network/main.tf
+++ terraform/modules/network/main.tf
@@ -32,7 +32,7 @@ resource "aws_security_group" "ec2_sg_reditos" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = [var.vpc_reditos]
   }
   egress {
     from_port   = 0
