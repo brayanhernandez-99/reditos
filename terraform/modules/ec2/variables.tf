57681ef85f1e7d32b411ff7a03ece0ce8c3aab34
brayanhernandez-99 <bhrestrepo_27@hotmail.com>
2025-06-05 11:43:03 -0500

    Fixing warning in variables

--- terraform/modules/ec2/variables.tf
+++ terraform/modules/ec2/variables.tf
@@ -1,20 +1,20 @@
-variable "ami_id" {
-  description = "ID de la AMI que se usará para lanzar la instancia EC2"
-  type        = string
-}
-
 variable "ec2_instance_type" {
   description = "Tipo de instancia EC2"
   type        = string
   default     = "t3.micro"
 }
 
-variable "public_subnet_reditos" {
-  description = "ID de la subred pública donde se lanzará la instancia EC2"
+variable "ami_id" {
+  description = "ID de la Amazon Machine Image (AMI) para la instancia EC2"
   type        = string
 }
 
 variable "ssh_key_name" {
   description = "Nombre del par de claves SSH para acceso a EC2"
+  type        = string
+}
+
+variable "public_subnet_reditos" {
+  description = "ID de la subred pública donde se lanzará la instancia EC2"
   type        = string
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -3,3 +3,60 @@ variable "region" {
   type        = string
   default     = "us-east-1"
 }
+
+variable "vpc_reditos" {
+  description = "CIDR block de la VPC para el proyecto Reditos"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "public_snet_reditos" {
+  description = "CIDR block de la subred pública en Reditos"
+  type        = string
+  default     = "10.0.1.0/24"
+}
+
+variable "private_snet_reditos" {
+  description = "CIDR block de la subred privada en Reditos"
+  type        = string
+  default     = "10.0.2.0/24"
+}
+
+variable "ec2_instance_type" {
+  description = "Tipo de instancia EC2"
+  type        = string
+  default     = "t3.micro"
+}
+
+variable "ami_id" {
+  description = "ID de la Amazon Machine Image (AMI) para la instancia EC2"
+  type        = string
+}
+
+variable "ssh_key_name" {
+  description = "Nombre del par de claves SSH para acceso a EC2"
+  type        = string
+}
+
+variable "db_name" {
+  description = "Nombre de la base de datos"
+  type        = string
+}
+
+variable "db_username" {
+  description = "Usuario administrador de la base de datos"
+  type        = string
+  sensitive   = true
+}
+
+variable "db_password" {
+  description = "Contraseña del usuario administrador de la base de datos"
+  type        = string
+  sensitive   = true
+}
+
+variable "db_instance_type" {
+  description = "Tipo de instancia para RDS"
+  type        = string
+  default     = "db.t3.micro"
+}
