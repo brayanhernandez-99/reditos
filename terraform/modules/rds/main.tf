2d6412339171178e1a1abd0948639c4f1c9b46c0
brayanhernandez-99 <bhrestrepo_27@hotmail.com>
2025-06-05 16:41:25 -0500

    Implement encrypt RDS

--- terraform/modules/rds/main.tf
+++ terraform/modules/rds/main.tf
@@ -11,6 +11,7 @@ resource "aws_db_instance" "db_reditos" {
   allocated_storage      = 20
   engine                 = "mysql"
   instance_class         = var.db_instance_type
+  storage_encrypted      = true
   name                   = var.db_name
   username               = var.db_username
   password               = var.db_password
