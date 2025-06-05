eed4992aa673217a1b2c1c4c2f65310f88d68288
brayanhernandez-99 <bhrestrepo_27@hotmail.com>
2025-06-04 21:10:31 -0500

    Change version Terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,12 +1,5 @@
 terraform {
-  required_version = ">= 1.3.0"
-
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
+  required_version = ">= 0.12"
 }
 
 module "network" {
